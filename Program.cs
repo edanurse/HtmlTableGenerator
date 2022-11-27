d8be30203679cf5d0a268962768437566aa3a94a
Edanur ŞENGÜR <edanurrsengur@gmail.com>
2022-11-27 15:50:38 +0300

    De-Serialize işlemi gerçekleştirildi.

--- Program.cs
+++ Program.cs
@@ -4,4 +4,5 @@ using System.Text.Json;
 
 string fileName = @"C:\Users\sengu\Desktop\HtmlTableGenerator\movies.json";
 string jsonString = File.ReadAllText(fileName);
+List<Movies> Movie = JsonSerializer.Deserialize<List<Movies>>(jsonString)!;
 
